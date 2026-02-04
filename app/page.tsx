c04c83c1cbfde2f50e6293187915feb5f58016b2
Priya-0217 <prilgupta017@gmail.com>
2026-02-04 12:33:18 +0530

    build

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Suspense } from 'react'
 import Hero from './_components/hero'
 import HoverDestinations from './_components/HoverDestinations'
 
 const page = () => {
   return (
     <div>
-          <Hero />
-           <HoverDestinations />
+      <Suspense fallback={<div className="min-h-screen" />}>
+        <Hero />
+      </Suspense>
+      <HoverDestinations />
     </div>
-  
   )
 }
 
